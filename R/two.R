db45a64b672027e3ad8721758e05c63037c77eb7
mario-catalano-sdg <user8@armani.bnmf101w0rkehjogyohogjxw5h.fx.internal.cloudapp.net>
2019-02-22 11:09:04 +0000

    Added function two

--- R/two.R
+++ R/two.R
@@ -1,3 +1,12 @@
+#' @title Function Two
+#' @param x : numeric
+#' @param ... : further optional parameters
+#'
+#' @return numeric
+#'
+#' @examples
+#' two (7)
+#' @export
 two <- function(x, ...){
   m = x + 2  
   return ( m ) 
